8dcd7846b9412f7d2280716b6ce55c4cf6f397ee
Chris Steinke <chrisjsteinke@gmail.com>
2020-01-14 23:05:16 -0800

    Added channelbuf.go example

---
+++ channelbuf.go
@@ -0,0 +1,19 @@
+//
+// gobyexample.com
+// channelbuf.go
+//
+package main
+
+import "fmt"
+
+func main() {
+
+	messages := make(chan string, 2)
+
+	messages <- "buffered"
+	messages <- "channel"
+
+	fmt.Println(<-messages)
+	fmt.Println(<-messages)
+
+}
