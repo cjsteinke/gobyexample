cfa26cb17974abf86b4233159e9575299f929023
Chris Steinke <chrisjsteinke@gmail.com>
2020-11-21 23:53:51 -0800

    Adding more examples

---
+++ numberparse.go
@@ -0,0 +1,48 @@
+package main
+
+//
+// The built-in package strconv provides 
+// number parsing.
+//
+import (
+    "fmt"
+    "strconv"
+)
+
+func main() {
+
+	// 
+	// With ParseFloat, this 64 tells how many bits
+	// of precision to parse.
+	//
+    f, _ := strconv.ParseFloat("1.234", 64)
+    fmt.Println(f)
+
+	//
+	// FOr ParseInt, the 0 means infer the base from
+	// the string. 64 requires that the result fit 
+	// in the64 bites. 
+	//
+    i, _ := strconv.ParseInt("123", 0, 64)
+    fmt.Println(i)
+
+	// ParseInt will recognize hex-formatted numbers.
+    d, _ := strconv.ParseInt("0x1c8", 0, 64)
+    fmt.Println(d)
+
+	// A ParseUint iss also available
+    u, _ := strconv.ParseUint("789", 0, 64)
+    fmt.Println(u)
+
+	//
+	// Atoi is a convenience funciton for basic
+	// base-10 int parsing.
+	//
+    k, _ := strconv.Atoi("135")
+    fmt.Println(k)
+
+	// Parse functions return an error on bad input.
+    _, e := strconv.Atoi("wat")
+    fmt.Println(e)
+
+}
